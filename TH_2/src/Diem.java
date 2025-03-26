a264b5096e9288acacf10bae4bab7c2ff2c23455
Bui Hai Dang <dangbui623@gmail.com>
2025-03-26 09:47:31 +0700

    2

---
+++ TH_2/src/Diem.java
@@ -0,0 +1,23 @@
+
+public class Diem {
+	float x;
+	float y;
+
+	public Diem() {
+	}
+
+	public Diem(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float kc(Diem d) {
+		float kc = (float) Math.sqrt((x-d.x)*(x-d.x)+(y-d.y)*(y-d.y));
+		return kc;
+	}
+
+	public void inDiem() {
+		System.out.println("Diem: ( "+ Diem.this.x + Diem.this.y+ " )");
+	}
+
+}
